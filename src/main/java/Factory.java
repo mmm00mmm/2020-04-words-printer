0b64262079665c4d01d80ef4abb7732b0f83ea65
mmm00mmm <54709088+mmm00mmm@users.noreply.github.com>
2020-04-15 17:09:55 +0900

    MainとFactoryを追加

--- src/main/java/Factory.java
+++ src/main/java/Factory.java
@@ -1,2 +1,41 @@
+import convert.Capitalize;
+import convert.Compress;
+import convert.Convert;
+import convert.Reverse;
+import output.Horizontal;
+import output.Output;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Factory {
+    public static void factory(List<String> inputs) {
+        List<String> word = Arrays.asList(inputs.get(0).split(" "));
+
+        inputs.forEach(rule -> convertFactory(word, rule));
+        //todo:戻り値戻ってこないからどうしよ
+
+    }
+
+    public static List<String> convertFactory(List<String> words, String rule) {
+        Convert convert = new Capitalize();
+        if (rule.equals("--capitalize")) {
+            convert = new Capitalize();
+        }
+        if (rule.equals("--compress")) {
+            convert = new Compress();
+        }
+        if (rule.equals("--reverse")) {
+            convert = new Reverse();
+        }
+        return convert.convert(words);
+    }
+
+    public static Output outputFactory(List<String> words, String rules) {
+        Output output;
+        if (rules.equals("--horizontal")) {
+            output = new Horizontal();
+        }
+        return null;
+    }
 }
