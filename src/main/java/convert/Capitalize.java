130099906d2c259a91a7bbf2a29005bf4407f9ae
mmm00mmm <54709088+mmm00mmm@users.noreply.github.com>
2020-04-15 21:30:29 +0900

    Capitalizeの中身実装

--- src/main/java/convert/Capitalize.java
+++ src/main/java/convert/Capitalize.java
@@ -1,10 +1,13 @@
 package convert;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Capitalize implements Convert {
     @Override
     public List<String> convert(List<String> word) {
-        return null;
+        return word.stream()
+                .map(s -> s.substring(0, 1).toUpperCase() + s.substring(1).toLowerCase())
+                .collect(Collectors.toList());
     }
 }
