8a64c751b70ea181c4d7a2d202f8b72548c6821e
mmm00mmm <54709088+mmm00mmm@users.noreply.github.com>
2020-04-16 21:37:28 +0900

    Compress実装

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+        Factory.factory(Arrays.asList("edward gooooorey", "--compress", "--horizontal"));
         Factory.factory(Arrays.asList("edward gorey", "--capitalize", "--horizontal"));
     }
 }
--- src/main/java/convert/Compress.java
+++ src/main/java/convert/Compress.java
@@ -1,10 +1,28 @@
 package convert;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Compress implements Convert {
     @Override
-    public List<String> convert(List<String> word) {
-        return null;
+    public List<String> convert(List<String> words) {
+        return words.stream()
+                .map(this::compress)
+                .collect(Collectors.toList());
+    }
+
+    private String compress(String word) {
+        String result = "";
+        List<String> wordList = Arrays.asList(word.split(""));
+
+        for (int i = wordList.size() - 1; 0 < i; i--) {
+            if (!wordList.get(i).equals(wordList.get(i - 1))) {
+                result = wordList.get(i) + result;
+            }
+        }
+        return wordList.get(0) + result;
     }
 }
